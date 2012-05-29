a2a5111d761f440de868c4e7fdc6f821195467bc
Darryl Pogue <dvpdiner2@gmail.com>
2012-05-28 23:38:25 -0700

    Detect Flash support (and version if possible).
    Closes #1.

--- app/views/rails_wink/wink/_wink.js
+++ app/views/rails_wink/wink/_wink.js
@@ -14,3 +14,26 @@ profile.browser.availWidth = window.screen.availWidth;
 profile.browser.availHeight = window.screen.availHeight;
 
 profile.modernizr = winkModernizr;
+
+
+/* Detect Flash support */
+profile.plugins.flash = (function() {
+    var hasFlash = false;
+    if (navigator.plugins && navigator.plugins.length) {
+      if (navigator.plugins['Shockwave Flash']) {
+        /* I think this is the only case where we can get the version number */
+        hasFlash = navigator.plugins['Shockwave Flash'].description;
+      }
+    } else if (navigator.mimeTypes && navigator.mimeTypes.length) {
+      var mimeType = navigator.mimeTypes['application/x-shockwave-flash'];
+      hasFlash = mimeType && mimeType.enabledPlugin;
+    } else {
+      try {
+        var ax = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
+        hasFlash = true;
+      } catch (e) {
+        // No flash
+      }
+    }
+    return hasFlash;
+})();
